a15323d4fa9df0f6486f0e30cb3759f06ce9500c
KIHA <flklc901@gmail.com>
2020-06-01 18:07:16 +0900

    roomService,Impl_수정_기하
    roomService,Impl_수정_기하

--- GoodPlace/src/main/java/com/kh/goodplace/room/model/service/RoomService.java
+++ GoodPlace/src/main/java/com/kh/goodplace/room/model/service/RoomService.java
@@ -1,5 +1,35 @@
 package com.kh.goodplace.room.model.service;
 
+import java.util.ArrayList;
+
+import com.kh.goodplace.common.model.vo.PageInfo;
+import com.kh.goodplace.room.model.vo.Room;
+
 public interface RoomService {
+	
+	//1.숙소리스트 조회용 서비스
+	//1_1. 숙소 총 갯수 조회용 서비스
+	int selectListCount();
+	
+	//1_2.요청한 페이지에 보여질 숙소 리스트 조회용 서비스
+	ArrayList<Room> selectRoomsList(PageInfo pi);
+	
+	//2.숙소등록 서비스
+	int insertRoom(Room r);
+	
+	//3.숙소 조회용 서비스
+	Room selectBoard(int rno);
+	
+	//4.숙소 삭제용 서비스
+	int deleteRoom(int rno);
+	
+	//5.숙소 수정용 서비스
+	int updateRoom(Room r);
+	
+	//6.숙소 휴면용 서비스
+	int restRoom(int rno);
+	
+	
+	
 
 }
--- GoodPlace/src/main/java/com/kh/goodplace/room/model/service/RoomServiceImpl.java
+++ GoodPlace/src/main/java/com/kh/goodplace/room/model/service/RoomServiceImpl.java
@@ -1,18 +1,64 @@
 package com.kh.goodplace.room.model.service;
 
+import java.util.ArrayList;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.kh.goodplace.common.model.vo.PageInfo;
 import com.kh.goodplace.room.model.dao.RoomDao;
+import com.kh.goodplace.room.model.vo.Room;
 
 @Service("rService")
-public class RoomServiceImpl {
+public class RoomServiceImpl implements RoomService{
 	
 	@Autowired
 	private SqlSessionTemplate sqlSession;
 	
 	@Autowired
 	private RoomDao rDao;
+
+	@Override
+	public int selectListCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public ArrayList<Room> selectRoomsList(PageInfo pi) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int insertRoom(Room r) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Room selectBoard(int rno) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int deleteRoom(int rno) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int updateRoom(Room r) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int restRoom(int rno) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
