1ecc68bd09e64737d6debdd8a9f7f6a93145b471
hjbrains2 <63648985+hjbrains2@users.noreply.github.com>
2020-06-01 18:15:31 +0900

    partner_memberService 메소드생성_하정

--- GoodPlace/src/main/java/com/kh/goodplace/member/model/service/MemberService.java
+++ GoodPlace/src/main/java/com/kh/goodplace/member/model/service/MemberService.java
@@ -1,8 +1,12 @@
 package com.kh.goodplace.member.model.service;
 
+import java.util.ArrayList;
+
 import com.kh.goodplace.member.model.vo.Member;
 
 public interface MemberService {
+	
+	
 	// 1. 로그인용 서비스
 	Member loginMember(Member m);
 	
@@ -17,5 +21,31 @@ public interface MemberService {
 	
 	// 5. 아이디 중복체크용 서비스(AJAX)
 	int idCheck(String userId);
+	
+	
+	//-------------------------------------------------------------------
+	
+	// 1. 파트너 수입 목록 조회용 서비스
+	ArrayList<Member> selectIncomeList();
+	
+	// 2. 파트너 계정정보 등록용 서비스
+	Member selectAccount(int usNo);
+	
+	// 3. 파트너 계정정보 수정용 서비스
+	Member insertAccount(int usNo);
+	
+	//-------------------------------------------------------------------
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 }
