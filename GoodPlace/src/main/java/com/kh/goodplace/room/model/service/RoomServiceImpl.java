6a801a5a3437a1a456489216c72f60d886c6058c
KIHA <flklc901@gmail.com>
2020-06-01 17:27:25 +0900

    roomServiceimpl_수정_기하
    roomServiceimpl_수정_기하

--- GoodPlace/src/main/java/com/kh/goodplace/room/model/service/RoomServiceImpl.java
+++ GoodPlace/src/main/java/com/kh/goodplace/room/model/service/RoomServiceImpl.java
@@ -1,5 +1,18 @@
 package com.kh.goodplace.room.model.service;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kh.goodplace.room.model.dao.RoomDao;
+
+@Service("rService")
 public class RoomServiceImpl {
+	
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	
+	@Autowired
+	private RoomDao rDao;
 
 }
