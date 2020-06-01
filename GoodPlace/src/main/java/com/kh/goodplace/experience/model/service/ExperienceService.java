bab52a9cfda5fa4d588396d6357fe55f1545444c
hjbrains2 <63648985+hjbrains2@users.noreply.github.com>
2020-06-01 18:00:01 +0900

    exp service메소드등록_하정

--- GoodPlace/src/main/java/com/kh/goodplace/experience/model/service/ExperienceService.java
+++ GoodPlace/src/main/java/com/kh/goodplace/experience/model/service/ExperienceService.java
@@ -1,5 +1,37 @@
 package com.kh.goodplace.experience.model.service;
 
+import java.util.ArrayList;
+
+import com.kh.goodplace.common.model.vo.PageInfo;
+import com.kh.goodplace.experience.model.vo.Experience;
+
 public interface ExperienceService {
+	
+	// 1. 전체 체험목록 조회용 서비스
+	// 1_1. 전체 체험 갯수 조회용 서비스 
+	int selectListExpCount();
+	
+	// 1_2. 요청한 페이지에 보여질 체험리스트 조회용 서비스
+	ArrayList<Experience> selectList(PageInfo pi);
+	
+	// 2. 체험 등록용 서비스
+	int insertExp();
+	
+	// 3. 체험 상세조회용 서비스 
+	Experience selectExp(int exNo);
+	
+	// 4. 체험 수정용 서비스
+	int updateExp(int exNo);
+	
+	// 5. 체험 휴면용 서비스
+	int restExp(int exNo);
+	
+	// 6. 체험 삭제용 서비스
+	int deleteExp(int exNo);
+	
+	// 7. 체험 반려건 재등록 요청 서비스
+	int updateReExp(int exNo);
+	
+	
 
 }
--- GoodPlace/src/main/java/com/kh/goodplace/experience/model/service/ExperienceServiceImpl.java
+++ GoodPlace/src/main/java/com/kh/goodplace/experience/model/service/ExperienceServiceImpl.java
@@ -1,5 +1,63 @@
 package com.kh.goodplace.experience.model.service;
 
-public class ExperienceServiceImpl {
+import java.util.ArrayList;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kh.goodplace.common.model.vo.PageInfo;
+import com.kh.goodplace.experience.model.dao.ExperienceDao;
+import com.kh.goodplace.experience.model.vo.Experience;
+
+
+@Service("expService")
+public class ExperienceServiceImpl implements ExperienceService {
+	
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	
+	@Autowired
+	private ExperienceDao expDao;
+
+	@Override
+	public int selectListExpCount() {
+		return 0;
+	}
+
+	@Override
+	public ArrayList<Experience> selectList(PageInfo pi) {
+		return null;
+	}
+
+	@Override
+	public int insertExp() {
+		return 0;
+	}
+
+	@Override
+	public Experience selectExp(int exNo) {
+		return null;
+	}
+
+	@Override
+	public int updateExp(int exNo) {
+		return 0;
+	}
+
+	@Override
+	public int restExp(int exNo) {
+		return 0;
+	}
+
+	@Override
+	public int deleteExp(int exNo) {
+		return 0;
+	}
+
+	@Override
+	public int updateReExp(int exNo) {
+		return 0;
+	}
 
 }
